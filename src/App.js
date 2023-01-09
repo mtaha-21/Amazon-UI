ca0879c7a074068b3d65f0ccd0a9dbdbf0217797
Taha <iamtahamurtaza@gmail.com>
2023-01-09 12:34:51 +0530

    Amazon UI & Shoppping cart using redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Checkout from "./components/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
